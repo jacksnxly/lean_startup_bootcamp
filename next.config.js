949d1207eed3d2002fb4c7b84b5c69e90bcc3568
jacksnxly <thejax.ly@gmail.com>
2025-04-28 14:53:24 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  experimental: {
-    fontLoaders: [
-      {
-        loader: '@next/font/google',
-        options: { timeout: 30000 }, // Increased timeout to 30 seconds
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
